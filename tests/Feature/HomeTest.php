f03735164da8f7c3114e430cc4515f6ebffd51f3
jetroot <lorean700@gmail.com>
2022-04-05 02:46:28 -0800

    add simple feature tests

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_search_for_a_query()
+    {
+        $query = 'tancredy';
+        $response = $this->get("/home", [
+            "search" => $query
+        ]);
+
+
+        $response->assertStatus(302);
+    }
+
+    public function test_login() {
+        $cin = "your cin"; // cin in user table
+        $password = "your password"; // password in user table
+
+        // $response = $this->withHeaders(['Authorization' => "Bearer $token"])
+        //     ->json('POST', '/login', [
+        //         '_token' => 
+        //         'cin' => $cin,
+        //         'password' => $password
+        //     ]);
+
+        // $response->assertStatus(201);
+
+        $response = $this->json('POST', '/login', [
+                // '_token' => csrf_token(),
+                'cin' => $cin,
+                'password' => $password
+            ]);
+
+
+        // print_r($response);
+        $response->assertStatus(204);
+    }
+}
